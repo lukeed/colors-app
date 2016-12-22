69c5bdc3342a7073d179a8c660b65dc242aa76e9
Luke Edwards <luke.edwards05@gmail.com>
2016-12-21 23:02:35 -0800

    scroll to #color top onclick

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -17,16 +17,30 @@ const App = ({ color }) => (
 	</div>
 );
 
-let elem;
+let elem, clor;
 function draw(str) {
 	elem = render(<App color={ str }/>, doc.body, elem);
+	clor = clor || doc.getElementById('color');
+}
+
+let rID;
+function scrollUp() {
+	if (clor.scrollTop <= 0) {
+		clor.scrollTop = 0;
+		cancelAnimationFrame(rID);
+	} else {
+		clor.scrollTop -= 40;
+		rID = requestAnimationFrame(scrollUp);
+	}
 }
 
 function handler() {
 	const hash = read();
 	draw(names.indexOf(hash) > -1 ? hash : all[0])
+	scrollUp();
 }
 
+// init && redraw
 win.onhashchange = handler;
 doc.addEventListener('DOMContentLoaded', handler);
 
