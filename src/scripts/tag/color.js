f4e6fc2d1a702a67bf6b16669997855864097680
Luke Edwards <luke.edwards05@gmail.com>
2016-12-21 23:02:28 -0800

    draw all color shades; repeat '500' at the top

--- src/scripts/tag/color.js
+++ src/scripts/tag/color.js
@@ -1,7 +1,24 @@
 import { h } from 'preact';
+import { all, bgc } from '../shared';
 
-export default ({ color }) => (
-	<ul id="color">
-		<li>THIS IS COLOR: { color }</li>
-	</ul>
+const Row = ({idx, hex}) => (
+	<li style={ bgc(hex) }>
+		<p>{ idx }</p>
+		<p>{ hex }</p>
+	</li>
 );
+
+export default ({ color }) => {
+	const obj = all[color];
+
+	return (
+		<ul id="color">
+			<Row idx="500" hex={ obj['500'] } />
+			{
+				Object.keys(obj).map(k => (
+					<Row idx={ k } hex={ obj[k] } />
+				))
+			}
+		</ul>
+	);
+};
