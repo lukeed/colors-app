de3566c37c8a8eb44c82344d84d235e9e88b88fe
Luke Edwards <luke.edwards05@gmail.com>
2016-12-21 20:09:43 -0800

    add top component & styles

--- src/scripts/tag/top.js
+++ src/scripts/tag/top.js
@@ -1,5 +1,5 @@
 import { h } from 'preact';
 
-export default ({ color }) => {
-	console.log('title received', color);
-};
+export default ({ color }) => (
+	<header id="title">{ color }</header>
+);
