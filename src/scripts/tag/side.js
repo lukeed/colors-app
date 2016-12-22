ee298a1b897c3a148b402747998d75300dead5bf
Luke Edwards <luke.edwards05@gmail.com>
2016-12-21 21:25:14 -0800

    attach scrollable nav/sidebar - use flex attr selectors

--- src/scripts/tag/side.js
+++ src/scripts/tag/side.js
@@ -1,5 +1,11 @@
 import { h } from 'preact';
+import Square from './square';
+import { all } from '../shared';
 
-export default ({ color }) => {
-	console.log('sidebar received', color);
-};
+export default ({ color }) => (
+	<nav id="side" dflex column>
+		{ all.map(s =>
+			<Square color={ s } active={ s === color } />
+		) }
+	</nav>
+);
---
+++ src/scripts/tag/square.js
@@ -0,0 +1,7 @@
+import { h } from 'preact';
+
+export default ({ color, active }) => {
+	let cls = `md-color-${color}-500`;
+	active && (cls += ' active');
+	return <a href={ `#/${color}` } className={ cls } flex></a>
+};
