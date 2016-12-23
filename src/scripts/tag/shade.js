84112354a43f9c102958de54d7a0bc1d9dba3eae
Luke Edwards <luke.edwards05@gmail.com>
2016-12-23 09:13:02 -0800

    copy color code ondblclick

--- src/scripts/tag/shade.js
+++ src/scripts/tag/shade.js
@@ -1,7 +1,16 @@
 import { h } from 'preact';
-import { bgc } from '../shared';
+import { bgc, doc } from '../shared';
 import { hex2rgb, isDark } from '../convert';
 // import { hex2rgb, rgb2hsl, isLight } from '../convert';
+
+function copy(e) {
+	const el = doc.createElement('input');
+	el.value = e.target.lastChild.textContent;
+	doc.body.appendChild(el);
+	el.select();
+	doc.execCommand('copy');
+	el.remove();
+}
 
 export default ({idx, hex}) => {
 	const rgb = hex2rgb(hex);
@@ -9,7 +18,7 @@ export default ({idx, hex}) => {
 	isDark(rgb) && (style += 'color:white;');
 
 	return (
-		<li style={ style }>
+		<li style={ style } onDblClick={ copy }>
 			<strong>{ idx }</strong>
 			<p>{ hex }</p>
 		</li>
