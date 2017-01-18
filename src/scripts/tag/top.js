b09c8f4191e241ae7087a718f16fcfb9c0149bbc
Luke Edwards <luke.edwards05@gmail.com>
2017-01-18 14:40:22 -0800

    place logo & mode in #top

--- src/scripts/tag/top.js
+++ src/scripts/tag/top.js
@@ -1,7 +1,15 @@
 import { h } from 'preact';
 
+function hello(e) {
+	console.log('hello', e);
+}
+
 export default ({ color }) => (
 	<header id="top">
+		<a id="logo" onClick={ hello }/>
 		<h1>{ color.replace('-', ' ') }</h1>
+		<div>
+			HEX
+		</div>
 	</header>
 );
