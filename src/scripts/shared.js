3b20ab9fec19fabe4ffbfcdd337beff39930e6dc
Luke Edwards <luke.edwards05@gmail.com>
2017-01-18 13:24:04 -0800

    add doc-based event handlers (on, off, emit)

--- src/scripts/shared.js
+++ src/scripts/shared.js
@@ -10,3 +10,16 @@ export function id(str) {
 }
 
 export const burst = id('burst');
+
+export function on(ev, handler) {
+	doc.addEventListener(ev, handler);
+}
+
+export function off(ev, handler) {
+	doc.removeEventListener(ev, handler);
+}
+
+export function emit(ev, detail) {
+	const evt = detail ? new CustomEvent(ev, { detail }) : new Event(ev);
+	doc.dispatchEvent(evt);
+}
