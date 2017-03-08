0d60b4525dcb5837eeaca5c5253fbff287e97f12
Luke Edwards <luke.edwards05@gmail.com>
2017-03-08 00:13:29 -0800

    export schemes as "zipped" objects - color keys matched to values

---
+++ src/scripts/colors/index.js
@@ -0,0 +1,25 @@
+import * as MD from './material';
+import * as OC from './opencolor';
+
+/**
+ * Pair Color values with Key names.
+ * - (color list length is variable)
+ */
+const pair = (colors, keys) => {
+	let i, k, len, out = {};
+	for (k in colors) {
+		out[k] = {};
+		len = colors[k].length;
+		for (i = 0; i < len; i++) {
+			out[k][keys[i]] = colors[k][i];
+		}
+	}
+	return out;
+};
+
+const format = obj => ({ names: obj.names, colors: pair(obj.all, obj.keys) });
+
+export default {
+	material: format(MD),
+	opencolor: format(OC)
+}
