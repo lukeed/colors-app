2e7187e2245b38fcc688f2bcd5c46b1de2dc51de
Luke Edwards <luke.edwards05@gmail.com>
2017-03-08 01:39:17 -0800

    cleanup Toast vars; update open class modifier

--- src/scripts/tag/toast.js
+++ src/scripts/tag/toast.js
@@ -1,16 +1,14 @@
 import { h, Component } from 'preact';
-import { on, win } from '../shared';
+import { on } from '../shared';
 
 export default class Toast extends Component {
 	constructor() {
-		super(arguments);
+		this.state = { open:false };
 
-		this.state = {open: false};
-
-		this.hideToast = () => this.setState({open: false});
+		this.hideToast = () => this.setState({ open:false });
 
 		this.showToast = () => {
-			this.setState({open: true});
+			this.setState({ open:true });
 			setTimeout(this.hideToast, 3000);
 		};
 	}
@@ -19,13 +17,13 @@ export default class Toast extends Component {
 		on('copied', this.showToast);
 	}
 
-	shouldComponentUpdate(_, { open }) {
-		return open !== this.state.open;
+	shouldComponentUpdate(_, state) {
+		return state.open !== this.state.open;
 	}
 
 	render(_, { open }) {
 		return (
-			<div id="toast" className={ open ? 'opened' : '' }>Copied!</div>
+			<div id="toast" className={{ open }}>Copied!</div>
 		);
 	}
 }
