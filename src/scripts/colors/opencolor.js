4a93ecd7268497b28992068eb404a97dfad4a1b1
Luke Edwards <luke.edwards05@gmail.com>
2017-03-08 00:12:13 -0800

    export color schemes' color keys & values separately

--- src/scripts/colors/opencolor.js
+++ src/scripts/colors/opencolor.js
@@ -3,7 +3,6 @@
  * @see https://yeun.github.io/open-color/
  */
 export const all = {
-	gray: ['f8f9fa', 'f1f3f5', 'e9ecef', 'dee2e6', 'ced4da', 'adb5bd', '868e96', '495057', '343a40', '212529'],
 	red: ['fff5f5', 'ffe3e3', 'ffc9c9', 'ffa8a8', 'ff8787', 'ff6b6b', 'fa5252', 'f03e3e', 'e03131', 'c92a2a'],
 	pink: ['fff0f6', 'ffdeeb', 'fcc2d7', 'faa2c1', 'f783ac', 'f06595', 'e64980', 'd6336c', 'c2255c', 'a61e4d'],
 	grape: ['f8f0fc', 'f3d9fa', 'eebefa', 'e599f7', 'da77f2', 'cc5de8', 'be4bdb', 'ae3ec9', '9c36b5', '862e9c'],
@@ -15,7 +14,10 @@ export const all = {
 	green: ['ebfbee', 'd3f9d8', 'b2f2bb', '8ce99a', '69db7c', '51cf66', '40c057', '37b24d', '2f9e44', '2b8a3e'],
 	lime: ['f4fce3', 'e9fac8', 'd8f5a2', 'c0eb75', 'a9e34b', '94d82d', '82c91e', '74b816', '66a80f', '5c940d'],
 	yellow: ['fff9db', 'fff3bf', 'ffec99', 'ffe066', 'ffd43b', 'fcc419', 'fab005', 'f59f00', 'f08c00', 'e67700'],
-	orange: ['fff4e6', 'ffe8cc', 'ffd8a8', 'ffc078', 'ffa94d', 'ff922b', 'fd7e14', 'f76707', 'e8590c', 'd9480f']
+	orange: ['fff4e6', 'ffe8cc', 'ffd8a8', 'ffc078', 'ffa94d', 'ff922b', 'fd7e14', 'f76707', 'e8590c', 'd9480f'],
+	gray: ['f8f9fa', 'f1f3f5', 'e9ecef', 'dee2e6', 'ced4da', 'adb5bd', '868e96', '495057', '343a40', '212529']
 };
 
 export const names = Object.keys(all);
+
+export const keys = ['0', '1', '2', '3', '4', '5','6', '7', '8', '9'];
